a5622b54c5582fe94de7066e2699090e27fd32cb
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-07-19 02:01:40 +0700

    .

--- main.js
+++ main.js
@@ -38,16 +38,35 @@ function getResData() {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      const { reservation_code, firstname } = data;
+      const { reservation_code, firstnames } = data;
       const res_input = document.getElementById("reservation").value;
       const name_input = document.getElementById("firstname").value;
 
-      if (res_input != reservation_code) {
-        alert("Wrong");
+      let verified = false;
+      for (let i = 0; i < parseInt(JSON.stringify(data.length)); i++) {
+        if (String(res_input) == JSON.stringify(data[i].reservation_code)) {
+          for (
+            let j = 0;
+            j < parseInt(JSON.stringify(data[i].firstnames.length));
+            j++
+          ) {
+            if (String(name_input) == JSON.stringify(data[i].firstnames[j])) {
+              alert("Success!");
+              verified = true;
+            }
+          }
+        }
       }
 
-      if (name_input != firstname) {
-        alert("Wrong");
+      if (verified == false) {
+        //alert("Wrong");
+        console.log(JSON.stringify(data[0].firstnames[0]));
+        console.log(String(name_input));
+        if (
+          !(String(name_input) == String(JSON.stringify(data[0].firstnames[0])))
+        ) {
+          console.log("yayy");
+        }
       }
     })
     .catch((error) => {
