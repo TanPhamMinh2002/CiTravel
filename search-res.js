939c2d116e8b32ecbb00ef651be3a22876d29c4e
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-07-19 23:09:53 +0700

    emailotp

---
+++ search-res.js
@@ -0,0 +1,165 @@
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+let logo = document.querySelector(".logo");
+
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
+//Nav Bar remove on scroll
+function doSomething() {
+  menu.classList.remove("fa-times");
+  menu.classList.remove("active");
+  navbar.classList.remove("active");
+  logo.classList.remove("active");
+}
+document.addEventListener("scroll", function (e) {
+  lastKnownScrollPosition = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function () {
+      doSomething();
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
+
+//Nav expands on click
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-times");
+  menu.classList.toggle("active");
+  navbar.classList.toggle("active");
+  logo.classList.toggle("active");
+});
+
+//fetch local API data
+const url = "data.json";
+function getResData() {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const { reservation_code, firstnames } = data;
+      const res_input = document.getElementById("reservation").value;
+      const name_input = document.getElementById("firstname").value;
+
+      let verified_res = false;
+      let verified_firstname = false;
+      for (let i = 0; i < parseInt(JSON.stringify(data.length)); i++) {
+        if (res_input == data[i].reservation_code) {
+          verified_res = true;
+          for (
+            let j = 0;
+            j < parseInt(JSON.stringify(data[i].firstnames.length));
+            j++
+          ) {
+            if (name_input == data[i].firstnames[j]) {
+              let otp = document.querySelector(".otp");
+              otp.classList.toggle("active");
+              verified_firstname = true;
+            }
+          }
+        }
+      }
+
+      //Error Message
+      let popup = document.querySelector("#popup");
+      if (res_input == "" || name_input == "") {
+        popup.classList.toggle("active");
+        document.getElementById("popup").innerText =
+          "Cannot proceed with blank input(s)";
+      } else {
+        if (!verified_res) {
+          popup.classList.toggle("active");
+          document.getElementById("popup").innerText =
+            "Invalid Reservation Code!";
+        } else {
+          if (!verified_firstname) {
+            popup.classList.toggle("active");
+            document.getElementById("popup").innerText = "Invalid First Name";
+          }
+        }
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+//Generate OTP
+const generateOtp = (optLength) => {
+  let otp = "";
+  for (let i = 0; i < optLength; i++) {
+    otp += Math.floor(Math.random() * 10);
+  }
+  return otp;
+};
+let otp_code = generateOtp(6);
+
+//SMTP Module
+var Email = {
+  send: function (a) {
+    return new Promise(function (n, e) {
+      (a.nocache = Math.floor(1e6 * Math.random() + 1)), (a.Action = "Send");
+      var t = JSON.stringify(a);
+      Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) {
+        n(e);
+      });
+    });
+  },
+  ajaxPost: function (e, n, t) {
+    var a = Email.createCORSRequest("POST", e);
+    a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"),
+      (a.onload = function () {
+        var e = a.responseText;
+        null != t && t(e);
+      }),
+      a.send(n);
+  },
+  ajax: function (e, n) {
+    var t = Email.createCORSRequest("GET", e);
+    (t.onload = function () {
+      var e = t.responseText;
+      null != n && n(e);
+    }),
+      t.send();
+  },
+  createCORSRequest: function (e, n) {
+    var t = new XMLHttpRequest();
+    return (
+      "withCredentials" in t
+        ? t.open(e, n, !0)
+        : "undefined" != typeof XDomainRequest
+        ? (t = new XDomainRequest()).open(e, n)
+        : (t = null),
+      t
+    );
+  },
+};
+//Email OTP
+let email_code_status = false;
+function emailotp() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "citravel@gmail.com",
+    Password: "A3430A884D6537392E4046A1C5B3A47371F4",
+    To: "tannedstone@gmail.com",
+    From: "citravel@gmail.com",
+    Subject: "CiTravel Email OTP Verification",
+    Body: `Your OTP is ${otp_code}. Please do not share with anyone!`,
+  })
+    .then((message) => alert(message))
+    .catch((error) => {
+      console.error(error);
+    })
+    .then((email_code_status = true));
+}
+if (email_code_status) {
+  otp_form.addEventListener("input", () => {
+    if (email_input.value.length == 6) {
+      confirm_btn.disabled = false;
+    } else {
+      confirm_btn.disabled = true;
+    }
+  });
+}
