f90c733eb049155d6229669af68e53319dd819c7
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-07-29 10:52:17 +0700

    Auto-generated commit [ci skip]

--- assets/js/mobileNav.js
+++ assets/js/mobileNav.js
@@ -2,16 +2,26 @@ let menu = document.querySelector("#menu-bar");
 let navbar = document.querySelector(".navbar");
 let logo = document.querySelector(".logo");
 let popup = document.querySelector("#popup");
-
+let language = document.querySelector(".language");
+let account = document.querySelector(".account_settings");
 let lastKnownScrollPosition = 0;
 let ticking = false;
 
+const link = window.location.href.split("/");
+if (link[link.length - 1] == "home" || link[link.length - 1] == "search-res") {
+  language.classList.add("active");
+  account.classList.remove("active");
+} else {
+  language.classList.remove("active");
+  account.classList.add("active");
+}
 //Nav Bar remove on scroll
 function doSomething() {
   menu.classList.remove("fa-times");
   menu.classList.remove("active");
   navbar.classList.remove("active");
   logo.classList.remove("active");
+  language.classList.remove("active");
 }
 document.addEventListener("scroll", function (e) {
   lastKnownScrollPosition = window.scrollY;
@@ -32,4 +42,9 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("active");
   navbar.classList.toggle("active");
   logo.classList.toggle("active");
+  if (
+    !(link[link.length - 1] == "home" || link[link.length - 1] == "search-res")
+  ) {
+    language.classList.toggle("active");
+  }
 });
