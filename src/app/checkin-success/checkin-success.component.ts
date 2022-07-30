099d52f07e5b8074e2d334835a9560649c59fb47
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-07-30 20:40:10 +0700

    .

--- src/app/checkin-success/checkin-success.component.ts
+++ src/app/checkin-success/checkin-success.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Renderer2, Inject  } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-checkin-success',
   templateUrl: './checkin-success.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckinSuccessComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer2: Renderer2,
+    @Inject(DOCUMENT) private _document: Document) { }
 
   ngOnInit(): void {
+    const s = this.renderer2.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'assets/js/checkinSuccess.js';
+    s.text = ``;
+    this.renderer2.appendChild(this._document.body, s);
   }
 
 }
---
+++ src/assets/js/checkinSuccess.js
@@ -0,0 +1,4 @@
+let checkBox = document.querySelector("#send");
+checkBox.addEventListener("click", function () {
+  checkBox.classList.toggle("active");
+});
