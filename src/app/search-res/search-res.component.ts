7373552c3ad8f8d7e228f3acbbe7c94c2a629236
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-08-01 00:48:21 +0700

    .

--- src/app/search-res/search-res.component.ts
+++ src/app/search-res/search-res.component.ts
@@ -4,7 +4,7 @@ import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-search-res',
   templateUrl: './search-res.component.html',
-  styleUrls: ['./search-res.component.css'],
+  styleUrls: ['search-res.component.css'],
 })
 export class SearchResComponent implements OnInit {
 
@@ -13,9 +13,14 @@ export class SearchResComponent implements OnInit {
     @Inject(DOCUMENT) private _document: Document) { }
 
   ngOnInit(): void {
+    const c = this.renderer2.createElement('script');
+    c.type = 'text/javascript';
+    c.src = 'assets/js/search-res/searchResCss.js';
+    c.text = ``;
+    this.renderer2.appendChild(this._document.body, c);
     const s = this.renderer2.createElement('script');
     s.type = 'text/javascript';
-    s.src = 'assets/js/booking-confirm.js';
+    s.src = 'assets/js/search-res/booking-confirm.js';
     s.text = ``;
     this.renderer2.appendChild(this._document.body, s);
   }
