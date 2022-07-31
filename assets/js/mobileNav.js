79bca5b00b31a1ae15437c908e16f3c4099b5043
Pham Minh Tan <tanphamminh2002@gmail.com>
2022-07-31 11:26:55 +0700

    Auto-generated commit [ci skip]

--- assets/js/mobileNav.js
+++ assets/js/mobileNav.js
@@ -4,16 +4,23 @@ let logo = document.querySelector(".logo");
 let popup = document.querySelector("#popup");
 let language = document.querySelector(".language");
 let account = document.querySelector(".account_settings");
+let accountMenu = document.querySelector(".accountMenu");
 let lastKnownScrollPosition = 0;
 let ticking = false;
 
 const link = window.location.href.split("/");
-if (link[link.length - 1] == "home" || link[link.length - 1] == "search-res") {
-  language.classList.add("active");
-  account.classList.remove("active");
-} else {
+if (
+  !(
+    link[link.length - 1] == "home" ||
+    link[link.length - 1] == "search-res" ||
+    link[link.length - 1] == "#"
+  )
+) {
   language.classList.remove("active");
   account.classList.add("active");
+} else {
+  language.classList.add("active");
+  account.classList.remove("active");
 }
 //Nav Bar remove on scroll
 function doSomething() {
@@ -21,6 +28,7 @@ function doSomething() {
   menu.classList.remove("active");
   navbar.classList.remove("active");
   logo.classList.remove("active");
+  accountMenu.classList.remove("active");
   if (link[link.length - 1] == "search-res") {
     document.querySelector(".qrScan").classList.remove("hide");
   }
@@ -30,7 +38,11 @@ function doSomething() {
     document.getElementById("send").classList.remove("hide");
   }
   if (
-    !(link[link.length - 1] == "home" || link[link.length - 1] == "search-res")
+    !(
+      link[link.length - 1] == "home" ||
+      link[link.length - 1] == "search-res" ||
+      link[link.length - 1] == "#"
+    )
   ) {
     language.classList.remove("active");
   }
@@ -55,8 +67,19 @@ menu.addEventListener("click", () => {
   navbar.classList.toggle("active");
   logo.classList.toggle("active");
   if (
-    !(link[link.length - 1] == "home" || link[link.length - 1] == "search-res")
+    !(
+      link[link.length - 1] == "home" ||
+      link[link.length - 1] == "search-res" ||
+      link[link.length - 1] == "#"
+    )
   ) {
     language.classList.toggle("active");
   }
 });
+
+function openList() {
+  accountMenu.classList.add("active");
+}
+function closeList() {
+  accountMenu.classList.remove("active");
+}
